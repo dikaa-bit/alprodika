b832ee8cd5d136720194add688b24dd272bbb0af
dikaa-bit <DIKARSKII90@GMAIL.COM>
2024-10-24 15:06:16 +0700

    Add files via upload

---
+++ math_modul.py
@@ -0,0 +1,12 @@
+import math
+
+akar = math.sqrt(25)
+pangkat = math.pow(5,4)
+bulat_bawah = math.floor(2.7)
+bulat_atas = math.ceil(2.7)
+print()           
+print(f"akar 15 = {akar}")
+print(f"5 pankat 2 = {pangkat}")
+print(f"pembulatan ke bawah dari 3.7 = {bulat_bawah}")
+print(f"pembulatan ke atas dari 3.7 = {bulat_atas}")
+print()
